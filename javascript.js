1bd3dffda3c9b6ac01205fb8c58a5b3e1fd58230
Blixa Morgan <blixa@projectmakeit.com>
2015-04-14 22:33:48 -0500

    add langs for mulilanguage support

--- javascript.js
+++ javascript.js
@@ -1,4 +1,14 @@
 #!/usr/bin/env node
+var fs = require('fs')
+var code = ""
+var lang = "English"
+var langFile = fs.readFileSync('./langs')
+
+for(line in langFile.toString().split('\n')){
+	if(langFile.toString().split('\n')[line].toString().split(':')[0]==lang){
+		code = langFile.toString().split('\n')[line].split(':')[1];
+	}
+}
 while(true){
-	console.log('code');
+	console.log(code);
 }
